e2652238677014c8c1b56cf6042b5d3359c235fd
Bob <240366771@qq.com>
2017-11-12 21:32:03 +0800

    201711122131

--- quan.py
+++ quan.py
@@ -4,31 +4,40 @@ import random
 import time
 import os
 import sys
-import shutil  
+import shutil
+# 转发内容1坐标
 an_box = (170,1250,1070,1500)
+# 转发内容2坐标
 an_box2 = (170,1650,1070,1900)
+# 任务栏坐标
 ex_box = (0,0,1080,71)
-#an_box2 = (116,842,720,942)
-#ios_box = (122,820,726,920)
+
+# 生成文件的名称和路径
 path = "E:/Py_Image/" +u"/朋友圈/微信图片" + str(int(time.time()))
+# 读取已有图像的路径
 dirpath = "E:\\Py_Image\\" + u"朋友圈".encode("gb2312")
+'''对保存文件目录进行检测'''
 if os.path.exists(dirpath):
 	shutil.rmtree(dirpath)
 	os.mkdir(dirpath)
 else:
 	os.mkdir(dirpath)
+'''读取转发素材并复制'''
 material = Image.open("E:/Py_Image/material/friend.jpg")
 m = material.crop(an_box)
+
 andorid = ["1","2","3","4","5","6","7","8",]
+# 其他内容文件名称（1-15）
 exans = []
 for x in xrange(1,15):
 	exans.append(str(x))
 	pass
-#IOS = ["1","2","3","4","5","6",]
+'''随机从中读取8个图像素材'''
 an = random.sample(andorid,8)
 exan = random.sample(exans,8)
-#ios = random.sample(IOS,4)
+
 s = 0
+# 批量替换和生成新的图像
 for a in an:
 	an_im = Image.open("E:/Py_Image/friends/Android/"+a+".jpg")
 	an_ex = Image.open("E:/Py_Image/friends/Android/eximage/" + exan[s] + ".jpg")
@@ -39,6 +48,10 @@ for a in an:
 	an_im.save(path + a + ".jpg","JPEG")
 	s = s + 1
 	pass
+
+#ios_box = (122,820,726,920)
+#IOS = ["1","2","3","4","5","6",]
+#ios = random.sample(IOS,4)
 #n = material.crop(an_box2)
 # for i in ios:
 # 	ios_im = Image.open("E:/Py_Image/friends/IOS/"+i+".jpg")
---
+++ start2.bat
@@ -0,0 +1,4 @@
+@echo off
+python quan.py
+python quns.py
+exit
