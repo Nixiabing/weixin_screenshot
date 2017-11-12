6526f5f4718488b9528991fed141dfab3f28fb74
Bob <240366771@qq.com>
2017-11-12 21:40:47 +0800

    201711122138

--- quan.py
+++ quan.py
@@ -9,7 +9,7 @@ an_box = (170,1250,1070,1500)
 an_box2 = (170,1650,1070,1900)
 ex_box = (0,0,1080,71)
 #an_box2 = (116,842,720,942)
-#ios_box = (122,820,726,920)
+
 path = "E:/Py_Image/" +u"/朋友圈/微信图片" + str(int(time.time()))
 dirpath = "E:\\Py_Image\\" + u"朋友圈".encode("gb2312")
 if os.path.exists(dirpath):
@@ -24,10 +24,10 @@ exans = []
 for x in xrange(1,15):
 	exans.append(str(x))
 	pass
-#IOS = ["1","2","3","4","5","6",]
+
 an = random.sample(andorid,8)
 exan = random.sample(exans,8)
-#ios = random.sample(IOS,4)
+
 s = 0
 for a in an:
 	an_im = Image.open("E:/Py_Image/friends/Android/"+a+".jpg")
@@ -39,6 +39,10 @@ for a in an:
 	an_im.save(path + a + ".jpg","JPEG")
 	s = s + 1
 	pass
+
+#ios_box = (122,820,726,920)
+#IOS = ["1","2","3","4","5","6",]
+#ios = random.sample(IOS,4)
 #n = material.crop(an_box2)
 # for i in ios:
 # 	ios_im = Image.open("E:/Py_Image/friends/IOS/"+i+".jpg")
